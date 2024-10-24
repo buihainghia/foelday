364e61733dbbdabe032729be3a43cb9a4d1a5460
buihainghiaa <86947656+buihainghia@users.noreply.github.com>
2024-10-24 21:44:16 +0700

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const userController = require('../controllers/userController')
 const { verifyTokenAndAuthorization } = require('../middleware/verifyToken')
 
-router.get('/', verifyTokenAndAuthorization, userController.getUser)
+router.get('/', userController.getUser)
 router.delete('/', userController.deleteUser)
 router.post('/verify/:otp', verifyTokenAndAuthorization, userController.verifyAccount)
 router.get('/verify_phone/:phone', verifyTokenAndAuthorization, userController.verifyPhone)
