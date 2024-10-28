fb0ac68f0aaebe9fe547f3199839093b3bf1314b
buihainghiaa <86947656+buihainghia@users.noreply.github.com>
2024-10-28 11:54:02 +0700

    Update foodController.js

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -66,7 +66,7 @@ module.exports = {
         try {
             const foods = await Food.find({ restaurant: id });
             if (foods.length === 0) {
-                return res.status(200).json("No food found for this restaurant");
+                return res.status(200).json([]);
             }
             return res.status(200).json(foods);
         } catch (error) {
