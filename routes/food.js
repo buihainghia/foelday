db0bcfe3ef2fe1c39cf8a209fe900aeaeb409aac
buihainghiaa <86947656+buihainghia@users.noreply.github.com>
2024-10-30 00:06:17 +0700

    Update food.js

--- routes/food.js
+++ routes/food.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const foodController = require('../controllers/foodController');
 const { verifyTokenAndVendor } = require('../middleware/verifyToken');
 
-router.post('/', foodController.addFood);
+router.post('/', verifyTokenAndVendor, foodController.addFood);
 router.get("/recommendation/:code", foodController.getRandomFood);
 router.get("/:id", foodController.getFoodById);
-router.get('/:restaurantId/:category', foodController.getFoodByRestaurantAndCategory);
+router.get('/category/:category', foodController.getFoodsBycategory);
 router.get("/restaurant-foods/:id", foodController.getFoodsByRestaurant);
 router.get("/search/:search", foodController.searchFoods);
-router.get("/:category/:code", foodController.getFoodsByCategoryAndCode);
+router.get("/:restaurantId/:category", verifyTokenAndVendor, foodController.getFoodByRestaurantAndCategory);
 router.get("/byCode/:code", foodController.getAllFoodsByCode);
 
 module.exports = router;
