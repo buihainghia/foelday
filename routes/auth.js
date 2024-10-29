10fb2f5df045dac73bd2b007acdaa3eae2ebc9f9
buihainghiaa <86947656+buihainghia@users.noreply.github.com>
2024-10-29 21:33:40 +0700

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 
 router.post('/register/:userType', authController.createUser);
-router.post('/login', authController.loginUser);
+router.post('/login/:userType', authController.loginUser);
 router.post('/logout', authController.logoutUser);
 router.post('/resendOTP', authController.reSendOtp);
 
